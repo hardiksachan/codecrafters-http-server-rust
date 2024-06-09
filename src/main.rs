f7a7370eddd25677dd43492db89a8f9f39e37d29
hardiksachan <sachanhardik@gmail.com>
2024-06-09 08:32:14 +0530

    echo handler

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ use std::{
     str,
 };
 
+type Error = Box<dyn std::error::Error>;
+type Result<T> = std::result::Result<T, Error>;
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -20,16 +23,55 @@ fn main() {
                 println!("accepted new connection");
                 let req = HttpRequest::from_incoming_stream(&mut stream);
                 match req.path.as_str() {
-                    "/" => stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap(),
-                    _ => stream
-                        .write("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes())
-                        .unwrap(),
-                };
+                    _ if req.path.starts_with("/echo/") => echo_handler(req.path),
+                    "/" => ok_handler(),
+                    _ => not_found_handler(),
+                }
+                .write(&mut stream)
+                .unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn echo_handler(path: String) -> HttpResponse {
+    let to_echo = path.strip_prefix("/echo/").unwrap();
+
+    let mut headers = HashMap::new();
+    headers.insert("Content-Length".to_owned(), format!("{}", to_echo.len()));
+    headers.insert("Content-Type".to_owned(), "text/plain".to_owned());
+    HttpResponse {
+        version: "HTTP/1.1".into(),
+        response_code: "200 OK".into(),
+        headers,
+        body: to_echo.into(),
+    }
+}
+
+fn ok_handler() -> HttpResponse {
+    let mut headers = HashMap::new();
+    headers.insert("Content-Length".to_owned(), format!("{}", 0));
+    headers.insert("Content-Type".to_owned(), "text/plain".to_owned());
+    HttpResponse {
+        version: "HTTP/1.1".into(),
+        response_code: "200 OK".into(),
+        headers,
+        body: "".into(),
+    }
+}
+
+fn not_found_handler() -> HttpResponse {
+    let mut headers = HashMap::new();
+    headers.insert("Content-Length".to_owned(), format!("{}", 0));
+    headers.insert("Content-Type".to_owned(), "text/plain".to_owned());
+    HttpResponse {
+        version: "HTTP/1.1".into(),
+        response_code: "404 Not Found".into(),
+        headers,
+        body: "".into(),
     }
 }
 
@@ -57,7 +99,7 @@ impl HttpRequest {
         let version = req_line.next().unwrap();
 
         let headers: HashMap<String, String> = HashMap::new();
-        while let Some(header) = lines.next() {
+        while let Some(_header) = lines.next() {
             // TODO: build headers
         }
 
@@ -69,5 +111,25 @@ impl HttpRequest {
             headers,
             body: body.to_owned(),
         }
+    }
+}
+
+struct HttpResponse {
+    version: String,
+    response_code: String,
+    headers: HashMap<String, String>,
+    body: String,
+}
+
+impl HttpResponse {
+    fn write(self, stream: &mut TcpStream) -> Result<()> {
+        let mut response = format!("{} {}\r\n", self.version, self.response_code);
+        for (k, v) in self.headers {
+            response += format!("{}: {}\r\n", k, v).as_str();
+        }
+        response += format!("\r\n{}", self.body).as_str();
+        stream.write(response.as_bytes())?;
+
+        Ok(())
     }
 }
